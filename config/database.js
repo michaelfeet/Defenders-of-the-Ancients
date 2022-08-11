62cbf3e4f69be63c2bb847ba540a89ad1f39f560
Michael Feet <michael.a.feet@gmail.com>
2022-08-11 19:28:19 -0400

    got to index page

--- config/database.js
+++ config/database.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
 // replace your database connection string here
-mongoose.connect(process.env.DATABASE_URL);
+// mongoose.connect('mongodb://localhost/movies');
+
+mongoose.connect(process.env.DATABASE_URL
+  // , {
+	// useNewUrlParser: true,
+	// useUnifiedTopology: true,
+	// useCreateIndex: true
+  // }
+  );
 
 const db = mongoose.connection;
 
--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,29 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //Require your User Model here!
-
+const User = require('../models/user');
 // configuring Passport!
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  function(accessToken, refreshToken, profile, cb) {
+  async function(accessToken, refreshToken, profile, cb) {
     // a user has logged in via OAuth!
     // refer to the lesson plan from earlier today in order to set this up
-
+    const user = await User.findOne({googleId: profile.id});
+    if (user) return cb(null, user);
+    try {
+      user = await User.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      });
+      return cb(null, user);
+    } catch (err) {
+      return cb(err);
+    }
   }
 ));
 
@@ -24,7 +36,9 @@ passport.deserializeUser(function(id, done) {
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
   // When you call this done function passport assigns the user document to req.user, which will 
   // be availible in every Single controller function, so you always know the logged in user
-
+  User.findById(userId).then(function(user) {
+    cb(null, user);
+  });
 });
 
 
