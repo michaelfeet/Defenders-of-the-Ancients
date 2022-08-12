bc41f60470d1ebaeffdb498d4c9032e02c510bf7
Michael Feet <michael.a.feet@gmail.com>
2022-08-12 12:52:50 -0400

    log in works

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
   // in the student demo this was res.redirect('/movies'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
   // a request to `/auth/google` route below
-  res.render('heros/index')
+  res.redirect('/heros')
 });
 
 // Google OAuth login route
@@ -22,15 +22,15 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/heros/index', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/index' //  UPDATE THIS, where do you want the client to go if login fails
+    successRedirect : '/heros', // UPDATE THIS, where do you want the client to go after you login 
+    failureRedirect : '/heros' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destroys the cookie keeping track of the user!
-    res.redirect('/index') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/heros') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
